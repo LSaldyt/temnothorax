9228a722e3eb5bde7160578f28996ae658c9ad6f
LSaldyt <lucassaldyt@gmail.com>
2018-10-30 12:54:22 -0700

    Fixes a sign error

--- pratt_2002/ode.py
+++ pratt_2002/ode.py
@@ -1,5 +1,10 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set_style('whitegrid')
+
 # SPratt Parameters from 2002 paper
-N = 208 # SD 99
+N = 208  # SD 99
 p = 0.25 # SD 0.1
 M = 2
 l   = [0.033] * M # SD 0.016
@@ -13,7 +18,7 @@ rho = {
 }
 
 #c = 4.6 # Minutes to move item.. not used
-T = 20 # ?
+T = 100 # ?
 
 def I(Ri, S):
     return Ri if Ri < T and S > 0 else 0
@@ -23,7 +28,7 @@ def J(Ri, Pinitial):
 
 def dS(S, R):
     return (-sum(ui * S for ui in u)
-            + sum(l[i] * I(R[i], S) for i in range(len(R))))
+            - sum(l[i] * I(R[i], S) for i in range(len(R))))
 
 def dAi(S, R, A, i):
     return (u[i] * S
@@ -37,19 +42,35 @@ def dRi(R, A, i):
     return (k[i]*A[i]
             + sum((rho[(j, i)]*R[j] -  rho[(i, j)]*R[i])
                   for j in range(len(A)) if j != i))
-def dPi(R, Pinitial, i):
-    return phi[i] * J(R[i], Pinitial)
 
+def dPi(R, Pinitial, i):
+    print('dPi debug:')
+    print(phi[i] * J(R[i], Pinitial))
+    print(J(R[i], Pinitial))
+    return phi[i] * J(R[i], Pinitial)
 
 def simulate():
     S = int(p * N)
-    Pinitial = [int((1.0 - p) * N)]
+    Pinitial = int((1.0 - p) * N)
     P = [0] * M
     A = [0] * M
     R = [0] * M
+
+    S_history = []
+    P_history = []
+    A_history = []
+    R_history = []
+
     print('Initial Population:')
     print('S: {S}\nA: {A}\nR: {R}\nP: {P}'.format(S=S, A=A, R=R, P=P))
-    while True:
+    #iterations = 143
+    iterations = 1000
+    for _ in range(iterations):
+        S_history.append(S)
+        P_history.append(P)
+        A_history.append(A)
+        R_history.append(R)
+
         newS = S + dS(S, R)
         newA = [Ai + dAi(S, R, A, i)     for i, Ai in enumerate(A)]
         newR = [Ri + dRi(R, A, i)        for i, Ri in enumerate(R)]
@@ -60,7 +81,26 @@ def simulate():
         P = newP # list(map(round, newP))
         print('Population:')
         print('S: {S}\nA: {A}\nR: {R}\nP: {P}'.format(S=S, A=A, R=R, P=P))
-        #1/0
+
+    time = list(range(iterations))
+    plt.plot(time, [N] * iterations, color='black', alpha=0.5, label='Total ant limit')
+    plt.plot(time, [int(N*p)] * iterations, '--', color='black', alpha=0.5, label='Active ant limit')
+    #plt.plot(time, [int(N*(1.0 - p))] * iterations, color='black', alpha=0.5, label='Passive ant limit')
+    plt.plot(time, S_history, label='Searching')
+    for i, Ah in enumerate(zip(*A_history)):
+        linestyle = ['-', '--'][i]
+        plt.plot(time, Ah, color='purple', label='Assessment {}'.format(i), linestyle=linestyle)
+    for i, Rh in enumerate(zip(*R_history)):
+        linestyle = ['-', '--'][i]
+        plt.plot(time, Rh, color='orange', label='Recruitment {}'.format(i), linestyle=linestyle)
+    for i, Ph in enumerate(zip(*P_history)):
+        linestyle = ['-', '--'][i]
+        plt.plot(time, Ph, color='green', label='Passive {}'.format(i), linestyle=linestyle)
+    plt.legend()
+    plt.xlabel('Minutes')
+    plt.ylabel('Population count')
+    plt.title('Population dynamics during nest search for Temnothorax')
+    plt.show()
 
 if __name__ == '__main__':
     simulate()
