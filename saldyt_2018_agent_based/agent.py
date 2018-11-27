88b07f5ead16cc94bd2fcce6e8819bd5a66fa336
LSaldyt <lucassaldyt@gmail.com>
2018-11-26 23:20:59 -0700

    Updates agent based model

--- saldyt_2018_agent_based/agent.py
+++ saldyt_2018_agent_based/agent.py
@@ -22,7 +22,7 @@ class Agent:
         self.commitment = weights[nest]
         self.current    = nest
 
-def transform(agents, agent, weights, free):
+def transform(agents, agent, weights, phi, free):
     if random() < agent.commitment:
         options = list(reduce(set.union, (s for k, s in free.items() if k != agent.current), set()))
         if options:
@@ -30,9 +30,9 @@ def transform(agents, agent, weights, free):
             recruited = agents[choice(options)]
             recruited.encounter(agent.current, weights)
     else:
-        for j, weight in enumerate(weights):
-            if random() < weight:
-                agent.encounter(j, weights)
+        for j, p in enumerate(phi):
+            if random() < p:
+                agent.encounter(j, phi)
     return agent
 
 def gen_free(agents):
@@ -43,15 +43,16 @@ def gen_free(agents):
 
 N = 208
 weights = [0.0, 0.015, 0.02]
+phi     = [0.0, 0.013, 0.013]
 M = len(weights)
 agents  = [Agent() for i in range(N)]
 free = gen_free(agents)
 
 history = defaultdict(list)
 
-iterations = 100
+iterations = 500
 for i in range(iterations):
-    agents = list(transform(agents, agent, weights, free) for agent in agents)
+    agents = list(transform(agents, agent, weights, phi, free) for agent in agents)
     free = gen_free(agents)
     for k in range(M):
         if k in free:
